413d4a86a4c8b1d6de8103986d2ae66a79d748d8
JackPetersen2 <98473133+JackPetersen2@users.noreply.github.com>
2022-02-08 18:39:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,21 +5,23 @@ import requests
 app = Flask(__name__)
 
 
-url = requests.get('https://www.reddit.com/r/Offensivejokes/').text
-
-soup = BeautifulSoup(url,'lxml')
 
 @app.route('/')
 def home():
+  for url in  urls:
+  response=requests.git(url)
+  soup=BeautifulSoup(response.content,'lxml')
+  
 
   memes = soup.find_all('div', class_='Post')
-  image_container=[]
-  for meme in memes:
-    image=meme.find('img')
-  image.append(image)
+  
+    for meme in memes:
+      image=meme.find('img')
+    image.append(image)
 
 
   return render_template("index.html", content=memes,image=image)
+
 if __name__ == '__main__':
   app.run()
 
