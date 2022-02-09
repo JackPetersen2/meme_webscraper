e90de6b23428a84388c3d7bd47aefc684065e4a5
JackPetersen2 <98473133+JackPetersen2@users.noreply.github.com>
2022-02-09 14:55:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 from bs4 import BeautifulSoup
 from flask import Flask,render_template
 import requests
+import re
 
 app = Flask(__name__)
 
-urls=['','']
+
+urls=['https://mui.com/components/material-icons/']
+
+
 
 @app.route('/')
 def home():
-  for url in  urls:
-  response=requests.git(url)
-  soup=BeautifulSoup(response.content,'lxml')
-  
-
-  memes = soup.find_all('div', class_='Post')
-  
-    for meme in memes:
-      image=meme.find('img')
-    image.append(image)
+  for url in urls:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'lxml')
 
 
-  return render_template("index.html", content=memes,image=image)
+    memes = soup.find_all('li', class_='css-cd6vjr')
+    
+    print(soup.prettify())
 
+
+    print(memes)
+
+    return render_template("index.html", memes=memes)
 if __name__ == '__main__':
   app.run()
+
+
+
 
 
